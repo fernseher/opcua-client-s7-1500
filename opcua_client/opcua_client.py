c354c8748ea25eb67b32931ca09ad4dafc324dc7
Maximilian Eder <fernseher@gmail.com>
2023-08-14 11:55:56 +0200

    Add call_method functionality Remove requirement of type specification at write(), by reading type from server first

--- opcua_client/opcua_client.py
+++ opcua_client/opcua_client.py
@@ -14,12 +14,6 @@ class OPCUAVariable:
 
 class OPCUAClient:
     client: Client
-    ua_types = {
-        'float': ua.VariantType.Float,
-        'int32': ua.VariantType.UInt32,
-        'str': ua.VariantType.String,
-        'byte': ua.VariantType.Byte,
-    }
 
     @classmethod
     async def create(
@@ -35,6 +29,7 @@ class OPCUAClient:
             self.client.set_password(password)
             # client.set_security_string()
             await self.client.connect()
+            await self.client.load_type_definitions()
         except Exception:
             raise  # TODO
         return self
@@ -46,18 +41,20 @@ class OPCUAClient:
 
     async def write(
             self,
-            opcua_var: OPCUAVariable,
+            variable_id: str,
             value: int | float | str,
     ) -> None:
-        var = self.client.get_node(opcua_var.node_id)
-        ua_type = self.ua_types[opcua_var.type]
+        var = self.client.get_node(variable_id)
+        data_type = await var.read_data_type_as_variant_type()
         try:
             # Send only data value without timestamps etc. because PLC does not accept otherwise
-            await var.set_value(ua.DataValue(ua.Variant(value, ua_type)))
+            await var.set_value(ua.DataValue(ua.Variant(value, data_type)))
         except ua.UaError:
-            logger.exception(f"Node with ID: {opcua_var.node_id} does not exist")
+            logger.exception(f"Node with ID: {variable_id} does not exist")
             return
 
-    # # TODO: Calling a method
-    # res = await client.nodes.objects.call_method(f"{nsidx}:ServerMethod", 5)
-    # print(f"Calling ServerMethod returned {res}")
+    async def call_method(self, node_id, *args):
+        node = self.client.get_node(node_id)
+        res = await node.call_method('Method', *args)
+        logger.info(f"Calling ServerMethod returned {res}")
+        return res
