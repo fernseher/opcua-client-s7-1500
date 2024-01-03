7cfdeda119e50ea367e4b69eef389dffd24fee48
Maximilian Eder <fernseher@gmail.com>
2024-01-03 22:55:09 +0100

    Remove project-specific data

--- README.md
+++ README.md
@@ -2,27 +2,27 @@
 
 
 ## Description
-Simple async OPCUA client wrapping the asyncua client to enable communication with our SIMATIC S7-1500 PLC of Wiring Robot
+Simple async OPCUA client wrapping the asyncua client to enable communication with SIMATIC S7-1500 PLC
 
 
-## Usage
+## Usage Example
 
 ```python
 from asyncua import Client
 from opcua_client import OPCUAClient
 
-url = "opc.tcp://192.168.100.127:4840"
-user = 'FAU'
-password = '12345678'
+url = "opc.tcp://$IP:4840"
+user = '$USER'
+password = '$PASSWORD'
 asyncua_client = Client(url)
 opcua_client = await OPCUAClient.create(asyncua_client, user, password)
 
 # Read value
-node_id = 'ns=3;s="DB_DVSData"."CD"."CyclicInputDataLE"."DVS_Status"'
+node_id = 'ns=3;s="DB_Motor"."CyclicInputData"."Status"'
 dvs_status = await opcua_client.read(node_id)
 
 # Write value
-speed_id = 'ns=3;s="DB_DVSData"."CD"."CyclicOutputDataBE"."Feed Speed"'
+node_id = 'ns=3;s="DB_Motor"."CyclicOutputData"."Speed"'
 speed = 10
 await opcua_client.write(speed_var, speed)
 ```
--- tests/integration.py
+++ tests/integration.py
@@ -4,24 +4,24 @@ from opcua_client import OPCUAClient
 
 
 async def main():
-    url = "opc.tcp://192.168.100.127:4840"
-    user = 'FAU'
-    password = '12345678'
+    url = "$URL"
+    user = '$USER'
+    password = '$PASSWORD'
     asyncua_client = Client(url)
     opcua_client = await OPCUAClient.create(asyncua_client, user, password)
 
     # Read value
-    node_id = 'ns=3;s="DB_DVSData"."CD"."CyclicInputDataLE"."DVS_Status"'
+    node_id = '$NODE_ID_1'
     dvs_status = await opcua_client.read(node_id)
     print(dvs_status)
 
     # Write value
-    speed_variable_id = 'ns=3;s="DB_DVSData"."CD"."CyclicOutputDataBE"."Feed Speed"'
+    speed_variable_id = '$NODE_ID_2'
     speed = 16
     await opcua_client.write(speed_variable_id, speed)
 
     # Call Method
-    node_id = 'ns=3;s="OPCAssembleUR10MoveL_DB"'
+    node_id = '$NODE_ID_3'
     target = ua.tfPose(0.1337, 0.1337, 0, 0, 0, 0)   # type: ignore
     tcp = ua.tfPose(0.1337, 0.1337, 0, 0, 0, 0)   # type: ignore
     speed = ua.Variant(0.01, ua.VariantType.Float)
